1cd515b3019042fcb88e8aba2a6fae9c71767245
MARCO KRIESSMANN <kriessmann-marco@s-giken.com>
2024-04-04 17:26:47 +0900

    Add project files.

---
+++ EntityFrameworkV3/Model.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkV3
+{
+    public class MyDBContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+        
+        
+        public string DbPath { get;}
+
+        public MyDBContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "product.db");
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+    => options.UseSqlite($"Data Source={DbPath}");
+    }
+
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Price { get; set; }
+        public int stock { get; set; }
+    }
+}
---
+++ EntityFrameworkV3/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.EntityFrameworkCore.Design;
+namespace EntityFrameworkV3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var dbPath = "mydatabase.db";
+
+            using (var context = new MyDBContext())
+            {
+                // Add a new product
+                var product = new Product()
+                {
+                    Id = 1,
+                    Name = "Product A",
+                    Description = "This is an awesome Product",
+                    Price = 100,
+                    stock = 10
+                };
+
+                context.Products.Add(product);
+                context.SaveChanges();
+
+                // Update a product
+                var productToUpdate = context.Products.FirstOrDefault(p => p.Name == "Product A");
+                if (productToUpdate != null)
+                {
+                    productToUpdate.Description = "Updated description";
+                    context.SaveChanges();
+                }
+
+                // Delete a product
+                var productToDelete = context.Products.FirstOrDefault(p => p.Name == "Product A");
+                if (productToDelete != null)
+                {
+                    context.Products.Remove(productToDelete);
+                    context.SaveChanges();
+                }
+
+            }
+
+        }
+
+
+        
+    }
+}
